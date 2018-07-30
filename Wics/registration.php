547c570a6c21c7fe9e797fb2fd7be647951e6fcc
BrutusBossNL <b.van.der.stelt.1191@gmail.com>
2018-07-30 11:16:49 +0200

    Fixed paths

--- Wics/registration.php
+++ Wics/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'WICS_Connector',
-    __DIR__
+    'Wics_Connector',
+    __DIR__.'/src'
 );
